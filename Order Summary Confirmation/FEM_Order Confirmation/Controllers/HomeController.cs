c4effbd1a613ee121a247db550fcf6818bf562bb
nymelchor <nymelchor@protonmail.com>
2021-10-28 15:29:47 -0700

    Add template file for MVC project

---
+++ Order Summary Confirmation/FEM_Order Confirmation/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using FEM_Order_Confirmation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FEM_Order_Confirmation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Order Summary Confirmation/FEM_Order Confirmation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FEM_Order_Confirmation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
